964c08d0bcc344e532ad1c5b0c3ce78411f74ccf
DustinSiegel <weenzombie@gmail.com>
2017-05-27 18:49:43 -0600

    just need to clean up comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,27 @@
- var moveStack = 0;
 var goalOptions = [24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61,61,];
 var moveOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var numberGoal = goalOptions[Math.floor(Math.random() * goalOptions.length)];
-var movePowerA = moveOptions[Math.floor(Math.random() * moveOptions.length)];
-var movePowerB = moveOptions[Math.floor(Math.random() * moveOptions.length)];
-var movePowerC = moveOptions[Math.floor(Math.random() * moveOptions.length)];
-var movePowerD = moveOptions[Math.floor(Math.random() * moveOptions.length)];
+var moveStack = 0;
+var numberGoal = 0;
+var movePowerA = 0;
+var movePowerB = 0;
+var movePowerC = 0;
+var movePowerD = 0;
 var wins = 0;
 var losses = 0;
 
-$("#moveGoal").text(numberGoal);
-$("#movesDone").text(moveStack);
 $("#winDisplay").text(wins);
 $("#lossDisplay").text(losses);
+
+$(".startGame").on("click", function() {
+	moveStack = 0;
+	numberGoal = goalOptions[Math.floor(Math.random() * goalOptions.length)];
+	movePowerA = moveOptions[Math.floor(Math.random() * moveOptions.length)];
+	movePowerB = moveOptions[Math.floor(Math.random() * moveOptions.length)];
+	movePowerC = moveOptions[Math.floor(Math.random() * moveOptions.length)];
+	movePowerD = moveOptions[Math.floor(Math.random() * moveOptions.length)];
+	$("#moveGoal").text(numberGoal);
+	$("#movesDone").text(moveStack);
+});
 
 $(".crimp").on("click", function() {
 	moveStack += movePowerA;
@@ -79,11 +88,4 @@ $(".handJam").on("click", function() {
     losses ++;
     $("#lossDisplay").text(losses);
     }
-});
-
-$(".startGame").on("click", function() {
-	numberGoal = goalOptions[Math.floor(Math.random() * goalOptions.length)];
-	moveStack = 0;
-	$("#moveGoal").text(numberGoal);
-	$("#movesDone").text(moveStack);
 });
